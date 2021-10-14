bd6e8f260927306dbf61dc94f76fc0599e473ae0
paulina0127 <paulina0127@yahoo.com>
2021-10-14 15:59:59 +0200

    Dodanie Lab1

---
+++ labs/src/Lab1.java
@@ -0,0 +1,90 @@
+// ZAD 1
+/*
+public class Lab1 {
+    public static void main(String[] args) {
+        System.out.println(31 + 29 + 31);
+    }
+}
+*/
+
+// ZAD 2
+/*
+public class Lab1 {
+    public static void main(String[] args) {
+        System.out.println(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10);
+    }
+}
+*/
+
+// ZAD 3
+/*
+public class Lab1 {
+    public static void main(String[] args) {
+        System.out.println(1 * 2 * 3 * 4 * 5 * 6 * 7 * 8 * 9 * 10);
+    }
+}
+*/
+
+// ZAD 4
+/*
+public class Lab1 {
+    public static void main(String[] args) {
+        int x = 1000;
+        double pierw = x * 0.06 + x;
+        double drugi = pierw * 0.06 + pierw;
+        double trzeci = drugi * 0.06 + drugi;
+        System.out.println("Pierwszy rok: " + pierw + "\n" + "Drugi rok: " + drugi + "\n" + "Trzeci rok: " + trzeci);
+    }
+}
+*/
+
+// ZAD 5
+/*
+public class Lab1 {
+    public static void main(String[] args) {
+        System.out.println("+------+");
+        System.out.println("| Java |");
+        System.out.println("+------+");
+    }
+}
+*/
+
+// ZAD 6
+/*
+public class Lab1 {
+    public static void main(String[] args) {
+        int znak = 0x0E04;
+        System.out.println("  /////  ");
+        System.out.println(" +" + (char)znak + (char)znak + (char)znak + (char)znak + (char)znak + "+ ");
+        System.out.println("(| ○○ |)");
+        System.out.println("  | ^ |  ");
+        System.out.println(" | '-' | ");
+        System.out.println(" +-----+ ");
+    }
+}
+*/
+
+// ZAD 10
+/*
+public class Lab1 {
+    public static void main(String[] args) {
+        System.out.println("1. Avengers" + "\n" + "2. Mulan" + "\n" + "3. Biały Wąż");
+    }
+}
+*/
+
+// ZAD 11
+/*
+public class Lab1 {
+    public static void main(String[] args) {
+        System.out.println("Wisława Szymborska - Nic dwa razy" + "\n");
+        System.out.println("Nic dwa razy się nie zdarza" + "\n" + "i nie zdarzy. Z tej przyczyny" + "\n" + "zrodziliśmy się bez wprawy" + "\n" + "i pomrzemy bez rutyny." + "\n");
+        System.out.println("Choćbyśmy uczniami byli" + "\n" + "najtępszymi w szkole świata," + "\n" + "nie będziemy repetować" + "\n" + "żadnej zimy ani lata." + "\n");
+        System.out.println("Żaden dzień się nie powtórzy," + "\n" + "nie ma dwóch podobnych nocy," + "\n" + "dwóch tych samych pocałunków," + "\n" + "dwóch jednakich spojrzeń w oczy." + "\n");
+        System.out.println("Wczoraj, kiedy twoje imię" + "\n" + "ktoś wymówił przy mnie głośno," + "\n" + "tak mi było jakby róża" + "\n" + "przez otwarte wpadła okno." + "\n");
+        System.out.println("Dziś, kiedy jesteśmy razem," + "\n" + "odwróciłam twarz ku ścianie." + "\n" + "Róża? Jak wygląda róża?" + "\n" + "Czy to kwiat? A może kamień?" + "\n");
+        System.out.println("Czemu ty się, zła godzino," + "\n" + "z niepotrzebnym mieszasz lękiem?" + "\n" + "Jesteś - a więc musisz minąć." + "\n" + "Miniesz - a więc to jest piękne." + "\n");
+        System.out.println("Uśmiechnięci, współobjęci" + "\n" + "spróbujemy szukać zgody," + "\n" + "choć różnimy się od siebie" + "\n" + "jak dwie krople czystej wody.");
+    }
+}
+*/
