627e9163a256a2d395cd941205f2b87c1c41037e
paulina0127 <73948761+paulina0127@users.noreply.github.com>
2022-01-13 22:09:09 +0100

    Zmiany w Lab4

--- labs/src/Lab4/Lab4_4.java
+++ labs/src/Lab4/Lab4_4.java
@@ -2,6 +2,7 @@ import java.math.BigInteger;
 
 public class Lab4_4 {
     public static void main(String[] args) {
+        // ZAD 4
         int n = Integer.parseInt(args[0]);
         BigInteger liczba = new BigInteger("0");
         BigInteger cnt = new BigInteger("0");
